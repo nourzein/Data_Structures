f7d6d44b2699410755886df28fc73aeb65683f7c
EC2 Default User <ec2-user@ip-172-31-46-46.us-east-2.compute.internal>
2019-09-23 17:27:57 +0000

    test code

--- week04.4.js
+++ week04.4.js
@@ -1,4 +1,6 @@
 const { Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // AWS RDS POSTGRESQL INSTANCE
 var db_credentials = new Object();
@@ -11,7 +13,7 @@ db_credentials.port = 5432;
 // Connect to the AWS RDS Postgres database
 const client = new Client(db_credentials);
 client.connect();
-console.log(client);
+// console.log(client);
 
 // Sample SQL statement to query the entire contents of a table: 
 var thisQuery = "SELECT * FROM aalocations;";
