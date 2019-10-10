3cef6303f3fa0a0e7c393cbe245a389cb97a0b59
EC2 Default User <ec2-user@ip-172-31-46-46.us-east-2.compute.internal>
2019-10-10 00:03:58 +0000

    parsing

--- week02/data_parsing.js
+++ week02/data_parsing.js
@@ -19,68 +19,70 @@ let aMeeting;
 $('tbody tbody tbody').children().each(function(i, elem) {
    
 
-$(elem).find("td").each(function(i, td) {
-    
-    if (i===0) { 
-  let html= $(td).html();
-  let brArray= html.split("<br>");
-  let address= brArray[2].split(",")[0].split(".")[0].split("-")[0].trim();
-  let meetingName= brArray[1].replace(/<.?b>/g,"").trim(); 
-  let meetingTitle= brArray[0].replace(/.....$/, "").split(">")[1].trim();
-  let fullLocation= brArray[2].trim() + "\\n" + brArray[3].trim();
-  let greyBox= $(td).find(".detailsBox").text().trim();   
-  let meetingInstances=[];
-    aMeeting= { 
-        address, meetingName, meetingTitle, fullLocation, greyBox, meetingInstances
-    
-         }
-
-}
-
-if (i === 1) {
-    let meetingInstances= $(td).html().split(/<br>\s+?<br>/).map(s=>s.trim());
-    meetingInstances.forEach(
-        (meetingInstance)=> {
-            console.log(meetingInstance.split(/<b>.*?<\/b>/));
-            let timeData= meetingInstance.split(/<b>.*?<\/b>/).filter(x=>x!== "")
-                .map(x=>{
-                    return x.trim().replace(/ <br>$/, "");
-                    
+    $(elem).find("td").each(function(i, td) {
+        
+      if (i===0) { 
+          let html= $(td).html();
+          let brArray= html.split("<br>");
+          let address= brArray[2].split(",")[0].split(".")[0].split("-")[0].trim();
+          let meetingName= brArray[1].replace(/<.?b>/g,"").trim(); 
+          let meetingTitle= brArray[0].replace(/.....$/, "").split(">")[1].trim();
+          let fullLocation= brArray[2].trim().replace("\n\t\t\t\t\t\t", "") + " " + brArray[3].trim();
+          let scheduleDetails= $(td).find(".detailsBox").text().trim();   
+          let meetingInstances=[];
+          if ((scheduleDetails)==='') return;      //why zero doesn't work but empty string does?
+            aMeeting= { 
+                address, meetingName, meetingTitle, fullLocation, scheduleDetails, meetingInstances
                 }
-                );
-            
+      }
+
+      if (i === 1) {
+        let meetingInstances= $(td).html().split(/<br>\s+?<br>/).map(s=>s.trim());
+        meetingInstances.forEach((meetingInstance)=> {
+            let timeData= meetingInstance.split(/<b>.*?<\/b>/).filter(x=>x!== "")
+              .map(x=>{
+               return x.trim().replace(/ <br>$/, ""); 
+                    }
+                    );
+            let dayN= meetingInstance.split('</b>')[0].replace(/.....$/, "").replace(/^.../, "");
             if (timeData.length === 0) return;
-            
+            if ((timeData[3])===0) return;
+            if ((timeData[3])===0) return;
             aMeeting.meetingInstances.push( {
-                day: $(td).html().split('</b>')[0].replace(/.....$/, "").replace("<b>", "").trim(),
+                day: dayN,
                 startTime: timeData[0],
                 endTime: timeData[1],
                 meetingType: (timeData[2]) ? timeData[2] : "",
-                specialInterest: (timeData[3]) ? timeData[3] : "",
-            }
-                );
-           
+                specialInterest: (timeData[3]) 
+                // ? timeData[3] : "",
+                    }
+                    );
         }
-        
         );
-          meetings.push(aMeeting);
-    // let day= string.split('</b>')[0].replace(/.....$/, "").replace("<b>", "").trim(); //why cant I use .replace(/...^?, "") to do it?
-    // let startTime= string.split('<b>')[1].replace("Mondays From</b>", "").trim();
-    // let endTime= string.split('<br>')[0].replace("<b>Mondays From</b>  3:00 PM <b>to</b>", "").trim();
-    // let meetingType= string.split('<br>')[1].replace("<b>Meeting Type</b>", "").trim();
-    // let specialInterest= string.split('<br>')[2].replace("<b>Special Interest</b>", "").trim();
-  
-//   console.log(aMeeting);  
-    // meetings.push({ 
-    // day, startTime, endTime, meetingType, specialInterest})
+      meetings.push(aMeeting);
     
-}
+     }
   
 });
-    console.log(meetings[i]);
+
+});
+
+//create IDs 
+const zone = 6;
+let id= (zone*1000)
+
+for (let i=0; i<meetings.length; i++) {
+    meetings[i]["locationId"]= id;
+ 
+ let meetingInstances = meetings[i].meetingInstances;
+//   console.log(id,meetingInstances);
+  for (let i=0;i<meetingInstances.length;i++) {
+      meetingInstances[i]["locationId"]=id;
+    //   console.log(id,meetingInstances[i])
+  }
+  
+   id += 1;
 }
-);
 
-// console.log(meetings);
+fs.writeFileSync('/home/ec2-user/environment/week02/results06.json', JSON.stringify(meetings));
 
-// fs.writeFileSync('/home/ec2-user/environment/week02/results06.json', JSON.stringify(meetings));
