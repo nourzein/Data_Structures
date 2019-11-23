8ad0011ac4a7c4a4b08f686ddb26aa854f82f586
EC2 Default User <ec2-user@ip-172-31-46-46.us-east-2.compute.internal>
2019-11-23 20:25:11 +0000

    week10 notes

---
+++ test/test.js
@@ -0,0 +1,164 @@
+//console.log("test")
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+const handlebars = require('handlebars')
+const fs = require('fs')
+
+
+//Configure Photon
+var device_id = process.env.PHOTON_ID;
+var access_token = process.env.PHOTON_TOKEN;
+var particle_variable = 'tempsensor';
+var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
+    
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+    db_credentials.user = 'nour'; //your username
+    db_credentials.host = process.env.WEB_HOST; //your endpoint
+    db_credentials.database = 'aa';
+    db_credentials.password = process.env.POSTGPW; //password. use an environment variable 
+    db_credentials.port = 5432; 
+
+//DB CONFIG
+var AWS = require('aws-sdk');
+AWS.config = new AWS.Config();
+AWS.config.region = "us-east-2";
+var dynamodb = new AWS.DynamoDB();    
+
+// //Install Express
+var express = require('express'), // npm install express
+    app = express();
+
+app.get('/sensor', function(req, res1) {
+    var templateVariables= { title: 'My Sensor Data', body: 'These are my temperatures in my room'};
+        //res.send('<h3>this is the page for my sensor data</h3>');  
+        
+    // Connect to the AWS RDS Postgres database
+    const client = new Client(db_credentials);
+    client.connect();
+    
+    var thisQuery = "SELECT * FROM sensorData;";
+    client.query(thisQuery, (err, res) => {
+        if (err) {throw err}
+        else {
+            
+            fs.readFile('./sensor.html', 'utf8', (error, data) => {
+                var template = handlebars.compile(data)
+                //console.log(templateVariables)
+                templateVariables.temperatures = res.rows;
+                //console.log(templateVariables)
+                var html = template(templateVariables)
+                res1.send(html)
+            // //console.table(res.rows);
+            // var rows= JSON.stringify(res.rows)
+            // res1.send(`<p> Rows ${rows} </p>`); 
+            //
+            
+            client.end();
+        });
+         client.end();
+        }
+    });
+
+});
+
+app.get('/aa',  function(req, res1) {
+    //res.send('<h3>this is the page for my sensor data</h3>');  
+    
+    // Connect to the AWS RDS Postgres database
+   var templateVariables= {};
+
+  
+    const client = new Client(db_credentials);
+    client.connect();
+    
+    var thisQuery = "SELECT * FROM aaallmeetings;";
+    client.query(thisQuery,   (err, res) => {
+        if (err) {throw err}
+        else {
+            
+            const data = res.rows;
+            
+            // start leaflet js
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            fs.readFile('./aa.hbs', 'utf8', (error, myData) => {
+                var template = handlebars.compile(myData, data)
+                // console.log(templateVariables)
+                templateVariables.blockofMeetings = data;
+                templateVariables.myData = JSON.stringify(data);
+                //console.log(templateVariables)
+                var html = template(templateVariables)
+                res1.send(html)
+            })
+
+        }
+        client.end();
+    });
+});
+
+
+
+
+
+//dear_diary query
+app.get('/processBlog', function(req, res1) {
+    
+ var templateVariables= {};
+ 
+    var params = {
+        TableName : "Dear_Diary",
+        KeyConditionExpression: 
+        "#tp = :topicName and dt between :minDate and :maxDate", // the query expression
+        ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+            "#tp" : "topic"
+        },
+        ExpressionAttributeValues: { // the query values
+            ":topicName": { S: "Data Structures Homework"},
+            ":minDate": {S: new Date("September 26, 2019").toLocaleString()},
+            ":maxDate": {S: new Date("September 28, 2019").toLocaleString()}
+        }
+    };
+    
+    
+    dynamodb.query(params, function(err, data2) {
+      if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+      } else {
+             
+             fs.readFile('./pblog.html', 'utf8', (error, data) => {
+                var template = handlebars.compile(data)
+                //console.log(templateVariables)
+                templateVariables.blogPost = data2.Items
+                console.log(templateVariables)
+                var html = template(templateVariables)
+                res1.send(html)
+            })
+         
+        // console.log("Query succeeded.");
+        // res1.send(JSON.stringify(data.Items, null, 4));
+        // .forEach(function(item) {
+        //   console.log("***** ***** ***** ***** ***** \n", item);
+        // });
+      }
+    });
+
+});
+
+// serve static files in /public
+app.use(express.static('public'));
+
+// listen on port 8080
+app.listen(8080, function() {
+    console.log('Server listening...');
+});
--- week11/README.md
+++ week11/README.md
@@ -2,11 +2,24 @@
 ## AA Meetings 
 In this sketch, I used a black background with a bright pink font. They are easy to read and the colors go well together. (I will admit I chose colors I liked more than colors that the audience would like because we are not graded for design but functionality)
 For functionality, I thought that indivuals will want to filter by day and time for meetings and have them show up on the map. I decided to have the bottom half of the side bar for when you click on the location and need more information about what meetings are there. This is so not to crowd the map.
+Will it be interactive? Yes- by filtering with day and time, and clicking to show meetings at a certain location.
+How will the data be mapped and what needs to happen for that? The data needs to be mapped as locaitons on a map with additional information. This has to be done using a dynamic query based on the filter of day and time. The extra info in the sidebar can be front-end js.
+The default view will be all the meetings with no filter. 
+The assumption I am making of the user is a) they are also a fan of the color pink, b) they are prioritizing day and time of meeting over meeting type and any special interets in the meeting.
 
 ## Dear Diary
 My diary will incude all kinds of experiences I am having in my three main data visualization courses. I included a fourth section that will have work that I am inspired by and wanted to share with people.
 I thought of having a cute side photo in each section about the general feel I have for the class. I want this blog to have a causal diary tone to it. I would like to develop it more in the future for myself since I've thought about blogging so a long time now and this is the perfect opportunity for it.
+Will it be interactive? Yes, by clicking the topic you want to explore.
+How will the data be mapped and what needs to happen? The query needs to be dynamic for the "topic" section and ordered by date--> I potentially might have the query display all the data and in the front end alter what shows when what button gets clicked- the data is small enough for that.
+The default view will be an introduction page of me introducing myself.
+The assumptions I make are that the people reading the blog are interested in the technical things that I am learning as well as my feelings through the process. It is not a professional blog, but a personal one. 
+
 
 ## Temperature Sensor
 For my temperature sensor, I originally thought of doing the days of the week and you can compare the temperature in one week on the clock (click day/night for am and pm); but since we are measuring temperature during a seasonal change, I decided to indclude a line (maybe bar) graph to show the whole month trend in temperature variation as well. 
 I might end up just with the line graph if time is limited because I do think month data will be conveying more information.
+Will it be interactive? Yes, their will be a tooltip when you hover on certain points on the line graph. Also, the clock will be interactive to switch betweem day/night time.
+How will it be mapped? The queries are ready. I need to figure out how to send two to my application for the two seperate graphs. They have all the information that I would display in the tooltip which I would do with front-end js. 
+The default view is the view I have sketched out with the line graph fully drawn and the clock shows the first day of recording. 
+The assumptions I make are that the view is intrigued by the temperature trend of the last couple of months, and also interested in exploring the days by themselves (the clock does not demonstrate a trend, it is more of an explorative tool for any user to have fun with).
