e463bda23f3100d164189c99b0b70ec251884f4a
EC2 Default User <ec2-user@ip-172-31-46-46.us-east-2.compute.internal>
2019-12-07 03:37:36 +0000

    sensor data

--- final_assignments/app.js
+++ final_assignments/app.js
@@ -32,8 +32,10 @@ AWS.config.region = "us-east-2";
 var dynamodb = new AWS.DynamoDB();    
 
 
-//setup
-// app.set('view engine', 'html');
+const indexSource = fs.readFileSync("public/sensor.hbs").toString();
+var template = handlebars.compile(indexSource, { strict: true });
+
+//setup paths 
 app.use(express.static(path.join(dir, '/public')));
 app.set('views', path.join(dir, 'public'));
 app.use(bodyParser.json());
@@ -47,17 +49,13 @@ app.get('/aa', function(req, res) {
     
 //route to query data
 app.post('/aaData',  function(req, res) {
-    //res.send('<h3>this is the page for my sensor data</h3>');  
-    // Connect to the AWS RDS Postgres database
-// var templateVariables= {};
+    const client = new Client(db_credentials);
+    client.connect();
 
-const client = new Client(db_credentials);
-client.connect();
-
+//variables for dynamic query
 var queryDay= req.body.queryDay
 // var [from, to]=req.body.queryTime.split(",");
 var queryTime= req.body.queryTime
-//queryTime= req.body.queryTime
 
     var thisQuery = "SELECT latitude, longitude, address, json_agg(json_build_object('Meeting Name', meetingname, 'Meeting Title', meetingtitle, 'Day', day, 'Start Time', starttime,  'End Time', endtime, 'Meeting Type', meetingtype, 'Special Interest', specialinterest, 'Address', fulllocation)) as meetings FROM aaallmeetings JOIN aaallmeetinginstances ON aaallmeetings.locationid = aaallmeetinginstances.locationid WHERE (1=1) " + queryDay +  queryTime + " GROUP BY latitude, longitude, address;";
     client.query(thisQuery, (err, results) => {
@@ -83,13 +81,12 @@ var queryTime= req.body.queryTime
     });
 });
 
-
-app.post('/aaMeetings',  function(req, res) {
-
-const client = new Client(db_credentials);
-client.connect();
-
-    var thisQuery = "SELECT latitude, longitude, address, json_agg(json_build_object('Meeting Name', meetingname, 'Meeting Title', meetingtitle, 'Day', day, 'Start Time', starttime,  'End Time', endtime, 'Metting Type', meetingtype, 'Special Interest', specialinterest, 'Address', fulllocation)) as meetings FROM aaallmeetings JOIN aaallmeetinginstances ON aaallmeetings.locationid = aaallmeetinginstances.locationid WHERE (1=1) GROUP BY latitude, longitude, address;";
+//request for default view of map with only lat and long
+app.get('/aaFullData',  function(req, res) {
+    
+    const client = new Client(db_credentials);
+    client.connect();
+var thisQuery = "SELECT DISTINCT latitude, longitude, address FROM aaallmeetings ;";
     client.query(thisQuery, (err, results) => {
         if (err) {throw err}
         else {
@@ -97,82 +94,46 @@ client.connect();
             // console.log(data)
               client.end();
               return res.json(data);
-        
         }
-        
     });
 });
 
 
-app.get('/aaFullData',  function(req, res) {
-    //res.send('<h3>this is the page for my sensor data</h3>');  
+app.get('/sensor', function(req, res1) {
+    //var templateVariables= { title: 'My Sensor Data', body: 'These are my temperatures in my room'};
+         
+        
     // Connect to the AWS RDS Postgres database
-// var templateVariables= {};
-
-const client = new Client(db_credentials);
-client.connect();
-
-    var thisQuery = "SELECT DISTINCT latitude, longitude, address FROM aaallmeetings ;";
-    client.query(thisQuery, (err, results) => {
+    const client = new Client(db_credentials);
+    client.connect();
+    
+  var thisQuery = "SELECT EXTRACT(MONTH FROM sensorTime) as sensormonth, EXTRACT(DAY FROM sensorTime) as sensorday, EXTRACT(HOUR FROM sensorTime) as sensorhour, AVG(sensorValue::int) as num_obs FROM sensorData GROUP BY sensormonth, sensorday, sensorhour ORDER BY sensormonth, sensorday, sensorhour;";;
+    client.query(thisQuery, (err, res) => {
         if (err) {throw err}
         else {
-            const data = results.rows;
-            // console.log(data)
-              client.end();
-              return res.json(data);
-            // start leaflet js
-            // fs.readFile('./aa.hbs', 'utf8', (error, myData) => {
-            //     var template = handlebars.compile(myData, data)
-            //     // console.log(templateVariables)
-            //     templateVariables.blockofMeetings = data;
-            //     templateVariables.myData = JSON.stringify(data);
+              res1.send(template({ sensordata: JSON.stringify(res.rows)}));
+            client.end();
+            console.log('1) responded to request for sensor graph');
+            // fs.readFile('./sensor.html', 'utf8', (error, data) => {
+            //     var template = handlebars.compile(data)
+            //     //console.log(templateVariables)
+            //     templateVariables.temperatures = res.rows;
             //     //console.log(templateVariables)
             //     var html = template(templateVariables)
-            //     res.send(html)
-            // })
+            //     res1.send(html)
+                                // // //console.table(res.rows);
+                                // // var rows= JSON.stringify(res.rows)
+                                // // res1.send(`<p> Rows ${rows} </p>`); 
+                                // //
+     // client.end();
+    // });
+         client.end();
         }
     });
+
 });
-
-
-// app.get('/sensor', function(req, res1) {
-//     var templateVariables= { title: 'My Sensor Data', body: 'These are my temperatures in my room'};
-//         //res.send('<h3>this is the page for my sensor data</h3>');  
-        
-//     // Connect to the AWS RDS Postgres database
-//     const client = new Client(db_credentials);
-//     client.connect();
-    
-//     var thisQuery = "SELECT * FROM sensorData;";
-//     client.query(thisQuery, (err, res) => {
-//         if (err) {throw err}
-//         else {
-            
-//             fs.readFile('./sensor.html', 'utf8', (error, data) => {
-//                 var template = handlebars.compile(data)
-//                 //console.log(templateVariables)
-//                 templateVariables.temperatures = res.rows;
-//                 //console.log(templateVariables)
-//                 var html = template(templateVariables)
-//                 res1.send(html)
-//             // //console.table(res.rows);
-//             // var rows= JSON.stringify(res.rows)
-//             // res1.send(`<p> Rows ${rows} </p>`); 
-//             //
-            
-//             client.end();
-//         });
-//          client.end();
-//         }
-//     });
-
-// });
-
-//dear_diary query
-//ask if req.query is the same as body parser 
   
  app.get('/dd', function(req, res1) {
- 
  var templateVariables= {}; 
  templateVariables.blogpost = [];
  var topic = 'Data Structures Homework' // default category
@@ -202,8 +163,6 @@ client.connect();
       if (err) {
           console.log('there was an error')
       } else {
-        // the information you want to send back to the browser goes in the res.send(). It can either be html (created either manually or via handlebars) or just JSON data.
-        // in this example it is the data from the database query
          //res.send(data)
         data2.Items.forEach(function(item){
             
@@ -224,6 +183,11 @@ client.connect();
 
 // serve static files in /public
 app.use(express.static('public'));
+
+//if not found
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
 
 // listen on port 8080
 app.listen(8080, function() {
